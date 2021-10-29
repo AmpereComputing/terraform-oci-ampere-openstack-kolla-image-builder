f8a34eeba25cc9ecfef6f01a88d1b6895a707f0e
Peter Pouliot <peter@pouliot.net>
2021-10-29 11:49:34 -0400

    initial commit

---
+++ docker_daemon.tf
@@ -0,0 +1,53 @@
+data "template_file" "docker_daemon_json" {
+  template = file("${path.module}/templates/docker-daemon.json.tpl")
+  vars = {
+    public_ip = oci_core_instance.ampere_a1.0.public_ip
+  }
+}
+
+output "docker_daemon_json" {
+  value = data.template_file.docker_daemon_json.rendered
+}
+
+data "template_file" "kolla_build_sh" {
+  template = file("${path.module}/templates/kolla_build.sh.tpl")
+  vars = {
+    public_ip = oci_core_instance.ampere_a1.0.public_ip
+  }
+}
+
+output "kolla_build_sh" {
+  value = data.template_file.kolla_build_sh.rendered
+}
+
+
+resource "null_resource" "configure_docker" {
+  triggers = {
+    instance_public_ip = oci_core_instance.ampere_a1.0.public_ip
+    template_content = data.template_file.docker_daemon_json.rendered
+  }
+  connection {
+    type        = "ssh"
+    host        = oci_core_instance.ampere_a1.0.public_ip
+    user        = "ubuntu"
+    private_key = tls_private_key.oci.private_key_pem
+  }
+
+  provisioner "file" {
+    content = data.template_file.docker_daemon_json.rendered
+    destination = "/home/ubuntu/daemon.json"
+  }
+
+  provisioner "file" {
+    content = data.template_file.kolla_build_sh.rendered
+    destination = "/home/ubuntu/kolla_build.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir /opt/kolla-build",
+      "sudo mv /home/ubuntu/kolla_build.sh /opt/kolla-build/",
+      "sudo chmod 0777 /opt/kolla-build/kolla_build.sh",
+    ]
+  }
+}
